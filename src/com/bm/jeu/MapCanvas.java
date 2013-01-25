0fb15bbcc6af975676c6ab96bc6011560bdeb7cd
Ben Magee <contact@bmagee.com>
2013-01-25 21:49:38 +0000

    First Drawing Attempt
    First attempt at drawing on a canvas.

--- src/com/bm/jeu/MapCanvas.java
+++ src/com/bm/jeu/MapCanvas.java
@@ -1,21 +1,37 @@
 package com.bm.jeu;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class MapCanvas extends JPanel implements MouseListener {
+	Graphics2D  graphics;
+	
 	public MapCanvas()
 	{
 		this.addMouseListener(this);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		graphics = (Graphics2D) g;
 	}
 	
 	public void initCanvas(int x, int y, int width, int height, Color bgColour)
 	{
 		setBounds(x, y, width, height);
 		setBackground(bgColour);
+	}
+	
+	public void drawPlayer(int x, int y, ImageIcon PlayerSprite)
+	{
+		graphics.drawLine(1, 3, 4, 5);
 	}
 	
 	@Override
