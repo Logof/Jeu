7839fc959cc9f6b0482d85ec2e7efb0c4c7057c6
BnMcG <contact@bmagee.com>
2013-03-24 21:04:52 +0000

    Commit fix

--- src/com/bm/jeu/net/NettyClient.java
+++ src/com/bm/jeu/net/NettyClient.java
@@ -1,6 +1,7 @@
 package com.bm.jeu.net;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
 
 import org.jboss.netty.bootstrap.ClientBootstrap;
@@ -12,51 +13,58 @@ public class NettyClient implements DefaultNetworkingClientServices {
 
 	private String HOST;
 	private int PORT;
-<<<<<<< HEAD
 	private boolean connectionStatus;
 
 	private ConcurrentLinkedQueue<String> outgoingQueue;
 	private ConcurrentLinkedQueue<String> incomingQueue;
 
-=======
->>>>>>> 5a535f776d5cc95a314ae7e33725306609851a1e
 	private ClientBootstrap bootstrap;
-	private Channel clientConnection;
-	private ChannelFuture lastWriteFuture = null;
+	private Channel connection;
 
 	public NettyClient(String host, int port) {
 		this.HOST = host;
 		this.PORT = port;
-<<<<<<< HEAD
 		outgoingQueue = new ConcurrentLinkedQueue<String>();
 		incomingQueue = new ConcurrentLinkedQueue<String>();
 		connectionStatus = false;
-=======
-		setup();
->>>>>>> 5a535f776d5cc95a314ae7e33725306609851a1e
 	}
 
 	private void setup() {
+		// Configure the client.
 		bootstrap = new ClientBootstrap(new NioClientSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
 
 		// Configure the pipeline factory.
 		bootstrap.setPipelineFactory(new NettyClientPipelineFactory());
-
 	}
 
 	@Override
-	public void connect() {
-		// Start the connection attempt.
-		ChannelFuture future = bootstrap.connect(new InetSocketAddress(HOST, PORT));
-		clientConnection = future.awaitUninterruptibly().getChannel();
-		if (!future.isSuccess()) {
-			future.getCause().printStackTrace();
-			bootstrap.releaseExternalResources();
+	public boolean connect() {
+		// uses the other connect statement to reduce work
+		return connect(HOST, PORT);
+	}
+
+	@Override
+	public boolean connect(String host, int port) {
+		// check if host and port are set
+		if (host != null && port > 0) {
+			//set up bootstrap
+			setup();
+
+			// Start the connection attempt.
+			ChannelFuture future = bootstrap.connect(new InetSocketAddress(host, port));
+
+			connection = future.awaitUninterruptibly().getChannel();
+			if (!future.isSuccess()) {
+				future.getCause().printStackTrace();
+				bootstrap.releaseExternalResources();
+			} else {
+				return true;
+			}
 		}
+		return false;
 	}
 
 	@Override
-<<<<<<< HEAD
 	public boolean disconnect() {
 		// Close the connection.  Make sure the close operation ends because
         // all I/O operations are asynchronous in Netty.
@@ -70,7 +78,7 @@ public class NettyClient implements DefaultNetworkingClientServices {
 
 	@Override
 	public boolean write(String message) {
-		
+
 		// Sends the received line to the server.
         connection.write(message);
 		return true;
@@ -85,38 +93,50 @@ public class NettyClient implements DefaultNetworkingClientServices {
 	public int getOutgoingQueueSize() {
 		return this.outgoingQueue.size();
 	}
-=======
-	public void connect(String host, int port) {
-		// TODO Auto-generated method stub
->>>>>>> 5a535f776d5cc95a314ae7e33725306609851a1e
 
+	@Override
+	public int getPort() {
+		return this.PORT;
+	}
+
+	// sets the new Port. you still need to reconnect to change the server on
+	// the connection
+	@Override
+	public void setPort(int port) {
+		this.PORT = port;
+
+	}
+
+	@Override
+	public String getHost() {
+		return this.HOST;
+	}
+
+	@Override
+	public void setHost(String host) {
+		this.HOST = host;
+
+	}
+
+	@Override
+	public boolean getConnectionStatus() {
+		return connectionStatus;
+	}
+
+	@Override
+	public String getNextIncomingItem() {
+		return this.incomingQueue.poll();
+	}
+
+	@Override
+	public String getNextOutgoingItem() {
+		return this.outgoingQueue.poll();
 	}
 
 	@Override
 	public void send(Object message) {
-		lastWriteFuture = clientConnection.write(message);
-
-	}
-
-	@Override
-	public void disconnect() {
 		// TODO Auto-generated method stub
-		// Wait until all messages are flushed before closing the channel.
-		if (lastWriteFuture != null) {
-			lastWriteFuture.awaitUninterruptibly();
-		}
-
-		// Close the connection. Make sure the close operation ends because
-		// all I/O operations are asynchronous in Netty.
-		clientConnection.close().awaitUninterruptibly();
-
-		// Shut down all thread pools to exit.
-		bootstrap.releaseExternalResources();
-
-	}
-	
-	public boolean isConnected(){
-		return clientConnection.isConnected();
+		
 	}
 
 }
--- src/com/bm/jeu/net/NettyClientHandler.java
+++ src/com/bm/jeu/net/NettyClientHandler.java
@@ -6,11 +6,10 @@ import java.util.logging.Logger;
 import org.jboss.netty.channel.ChannelEvent;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.Channels;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
-
-import com.bm.jeu.net.helpers.Component;
 
 public class NettyClientHandler extends SimpleChannelHandler {
 
@@ -21,35 +20,33 @@ public class NettyClientHandler extends SimpleChannelHandler {
 	@Override
 	public void handleUpstream(ChannelHandlerContext ctx, ChannelEvent e) throws Exception {
 		if (e instanceof ChannelStateEvent) {
-			// logger.info(e.toString());
+			logger.info(e.toString());
 		}
-<<<<<<< HEAD
 		Networking.getinstance();
-=======
->>>>>>> 5a535f776d5cc95a314ae7e33725306609851a1e
 		super.handleUpstream(ctx, e);
 	}
 
+	//this acts as soon as something wants to write inte the channel (from everywhere)
 	@Override
-	public void handleDownstream(ChannelHandlerContext ctx, ChannelEvent e) throws Exception {
+	public void writeRequested(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+		String message = (String) e.getMessage();
+		StringBuilder output = new StringBuilder();
+		//here will the commands be parsed or processed (except the dis/-connect one)
+		//put it together into the output one, so the server can understand it
 
-		// Log all channel state changes.
-		if (e instanceof MessageEvent) {
-			// logger.info("Writing:: " + e);
-		}
+		//TODO: parse commands
 
-		super.handleDownstream(ctx, e);
+		Channels.write(ctx.getChannel(), output.toString());
 	}
 
-	// Acts when a message was received
 	@Override
-	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
-//		System.out.println(e.getMessage());
-		if (e.getMessage() instanceof Component) {
-//			logger.info("Recieved:: " + (Component) e);
-			ComponentRecievedHandler.fireDataChange((Component) e.getMessage());
-		}
-		super.messageReceived(ctx, e);
+	public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+
+	}
+
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+		System.err.println(e.getMessage());
 	}
 
 	@Override
